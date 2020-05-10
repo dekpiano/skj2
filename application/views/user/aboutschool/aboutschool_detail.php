0d62d6b4e2ced05c0175ddb6effdc8afe9e3785a
Dekpiano <dekpiano4815@gmail.com>
2020-05-10 23:22:59 +0700

    หกด

--- application/views/user/aboutschool/aboutschool_detail.php
+++ application/views/user/aboutschool/aboutschool_detail.php
@@ -1,37 +1,48 @@
-<section id="breadcrumbs" class="breadcrumbs" style="background: #e9ebee;">
-		
-		<div class="container ">
-<div class="row">
-<div class="col-lg-8">
-	<div class="card border-5 shadow mb-4">
-	 	<div class="card-body">
-			<h5 class="m-0"><?=$about[0]->about_menu;?></h5>
-			<hr>
-			<?=$about[0]->about_detail;?>
-		</div>
-	</div>
-</div>
-<div class="col-lg-4">
-
-<div class="card border-2 shadow mb-4">
-	 <div class="card-body">
-	 	<h5 class="mb-0">เกี่ยวกับโรงเรียน</h5>
-	 	<br>
-	 	<?php foreach ($Allabout as $key => $v_Allabout) : ?>
-		<div class="d-flex justify-content-between small">
-			<a href="<?=base_url('AboutSchool/').$v_Allabout->about_id;?>" style="">
-			<h6 class="font-weight-bold"><?=$v_Allabout->about_menu;?></h6>
-			</a>
-		</div>
-		<hr class="my-0 mb-2">
-		<?php endforeach; ?>
-	
-	</div>
+<div class="section layout_padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="full center">
+                    <div class="heading_main text_align_center">
+                        <h2><span class="theme_color">เกี่ยวกับ</span> โรงเรียน</h2>
+                        <p class="large">โรงเรียนสวนกุหลาบวิทยาลัย (จิรประวัติ) นครสวรรค์</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
+<div class="container ">
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="card border-5 shadow mb-4">
+                <div class="card-body">
+                    <h5 class="m-0"><?=$about[0]->about_menu;?></h5>
+                    <hr>
+                    <?=$about[0]->about_detail;?>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
 
-</div>
-</div>
-</div>
+            <div class="card border-2 shadow mb-4">
+                <div class="card-body">
+                    <h5 class="mb-0">เกี่ยวกับโรงเรียน</h5>
+                    <br>
+                    <?php foreach ($Allabout as $key => $v_Allabout) : ?>
+                    <div class="d-flex justify-content-between small">
+                        <a href="<?=base_url('AboutSchool/').$v_Allabout->about_id;?>" style="">
+                            <h6 class="font-weight-bold"><?=$v_Allabout->about_menu;?></h6>
+                        </a>
+                    </div>
+                    <hr class="my-0 mb-2">
+                    <?php endforeach; ?>
 
-</section>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
--- application/views/user/album/album_main.php
+++ application/views/user/album/album_main.php
@@ -1,32 +1,33 @@
-
-<section id="breadcrumbs" class="portfolio" style="background: #e9ebee;">
-
+<div class="section layout_padding">
     <div class="container">
-        <!-- -fluid -->
-        <center>
-            <h1>รูปภาพกิจกรรมทั้งหมด</h1>
-            <p>โรงเรียนสวนกุหลาบวิทยาลัย (จิรประวัติ) นครสวรรค์</p>
-        </center>
-
-
-        <div class="card-columns">
-            <?php foreach ($images as $key => $v_images) : ?>
-            <div class="card">
-                <a target="_blank" title="<?=$v_images->img_title;?>" href="<?=$v_images->img_link;?>">
-                    <div class="img-hover-zoom img-hover-zoom--basic">
-                        <img class="card-img-top " src="<?=$v_images->img_mainpic;?>" alt="<?=$v_images->img_title;?>"
-                            style="width:100%">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="full center">
+                    <div class="heading_main text_align_center">
+                        <h2><span class="theme_color">รูปภาพกิจกรรม</span> ทั้งหมด</h2>
+                        <p class="large">โรงเรียนสวนกุหลาบวิทยาลัย (จิรประวัติ) นครสวรรค์</p>
                     </div>
-                    <div class="card-body text-center">
-                        <?=$v_images->img_title?>
-                </a>
+                </div>
             </div>
         </div>
-        <?php endforeach; ?>
     </div>
+</div>
 
+<div class="container">
+    <div class="card-columns">
+        <?php foreach ($images as $key => $v_images) : ?>
+        <div class="card">
+            <a target="_blank" title="<?=$v_images->img_title;?>" href="<?=$v_images->img_link;?>">
+                <div class="img-hover-zoom img-hover-zoom--basic">
+                    <img class="card-img-top " src="<?=$v_images->img_mainpic;?>" alt="<?=$v_images->img_title;?>"
+                        style="width:100%">
+                </div>
+                <div class="card-body text-center">
+                    <?=$v_images->img_title?>
+            </a>
+        </div>
     </div>
+    <?php endforeach; ?>
+</div>
 
-
-
-</section>
+</div>
